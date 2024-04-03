043b9d3a0e2c997a4905e10055ba4265f8200443
karanpatil1604 <karanpatil1604@gmail.com>
2024-04-03 23:42:06 +0530

    Separation of components in the navbar

--- src/components/Navbar/NavBar.vue
+++ src/components/Navbar/NavBar.vue
@@ -1,61 +1,19 @@
 <script setup>
 import SearchBar from '@/components/Navbar/SearchBar.vue'
+import BrandLogo from '@/components/Navbar/BrandLogo.vue'
+import NavMenue from '@/components/Navbar/NavMenue.vue'
 </script>
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <div class="navbar-brand" href="#">Logo</div>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarScroll"
-        aria-controls="navbarScroll"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarScroll">
-        <ul
-          class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
-          style="--bs-scroll-height: 100px"
-        >
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Sections</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Books</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Users</a>
-          </li>
-
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Account
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Register</a></li>
-              <li><a class="dropdown-item" href="#">Login</a></li>
-              <li><a class="dropdown-item" href="#">Profile</a></li>
-              <li><hr class="dropdown-divider" /></li>
-              <li><a class="dropdown-item" href="#">Logout</a></li>
-            </ul>
-          </li>
-        </ul>
+      <BrandLogo />
+      <NavMenue>
         <SearchBar />
-      </div>
+      </NavMenue>
     </div>
   </nav>
+  <main>
+    <router-view> This is a router view </router-view>
+  </main>
 </template>
 <style scoped></style>
