9b0e1748bee083be08b67356f43506fa9d3d974d
karanpatil1604 <karanpatil1604@gmail.com>
2024-04-05 22:01:44 +0530

    Added charts.js and sample charts on dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,27 @@
-<script setup></script>
+<script setup>
+import BarChart from '@/components/Charts/BarChart.vue'
+import ScatterChart from '@/components/Charts/ScatterChart.vue'
+</script>
 
 <template>
   <main>
-    <div class="border border-danger mt-2 mx-2 px-2 text-center">
-      <div class="row my-2 gap-2" style="height: 250px">
-        <div class="col bg-warning rounded-0">One</div>
-        <div class="col bg-success rounded-0">Two</div>
-        <div class="col bg-danger rounded-0">Three</div>
-        <div class="col bg-primary rounded-0">Four</div>
+    <div class="mt-2 mx-2 px-2 text-center">
+      <div class="row my-2" style="height: 200px">
+        <div class="col-sm-12 col-md-3 bg-warning rounded-0">One</div>
+        <div class="col-sm-12 col-md-3 bg-success rounded-0">Two</div>
+        <div class="col-sm-12 col-md-3 bg-danger rounded-0">Three</div>
+        <div class="col-sm-12 col-md-3 bg-primary rounded-0">Four</div>
       </div>
-      <div class="row my-2 gap-2" style="height: 375px">
-        <div class="col bg-success">Two</div>
-        <div class="col bg-primary">Four</div>
+      <div class="row my-2 gap-2" style="height: 400px">
+        <div class="col border border-success rounded-1">
+          <BarChart
+            aria-label="Sales figures for the years 2022 to 2024. Sales in 2022: 987, Sales in 2023: 1209, Sales in 2024: 825."
+            ref="bar"
+          ></BarChart>
+        </div>
+        <div class="col">
+          <ScatterChart></ScatterChart>
+        </div>
       </div>
     </div>
   </main>
