debee4dd1d22125b5bbc0302f488f8d89c366c4d
karanpatil1604 <karanpatil1604@gmail.com>
2024-04-08 19:02:24 +0530

    Added Login Form component and Link and View for the same

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import LoginForm from '@/components/Forms/LoginForm.vue'
+</script>
+<template>
+  <div class="container-fluid">
+    <div class="row mt-4">
+      <div class="col-md-3"></div>
+      <div class="col-md-6 col-sm-12">
+        <LoginForm></LoginForm>
+      </div>
+    </div>
+    <div class="col-md-3"></div>
+  </div>
+</template>
+<style scoped></style>
